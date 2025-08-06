7b3749e90e956f956dce4892d5769a3d45522c51
Luvkuxh <jainuniluvkush@gmail.com>
2025-08-06 23:10:42 +0530

    understand diff btw bundlers eg vite and normal react lib

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+    <h1> CHAI AUR REACT || By Luvkush Sharma </h1>
+
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+   <h1> chai and react in vite bundler by luvkush </h1>
+  )
+}
+
+export default App
